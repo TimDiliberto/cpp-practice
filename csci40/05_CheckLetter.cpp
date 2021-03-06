de119ae11c2f58e1bd0122faabcb8de917ec64c0
Timbo <timothydiliberto19@gmail.com>
2021-03-06 07:20:24 -0800

    Began csci40 problem 5
    Create and begin 05_CheckLetter.cpp problem. Will tackle after nap

---
+++ csci40/05_CheckLetter.cpp
@@ -0,0 +1,14 @@
+/**
+ * Program: CheckLetter.cpp
+ * Written by: Timothy Diliberto
+ * Adds a check letter to each 8-digit number. By splitting into 4 two-digit
+ *     numbers and summing those numbers, the remainder after division by 26
+ *     will indicate which letter corresponds with the number.
+ * Last modified: 3/6/2021
+ */
+
+#include <iostream>
+
+int main() {
+
+}
