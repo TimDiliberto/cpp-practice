d9d30e124f861da271eb8d91a37902422bfa822c
Timbo <timothydiliberto19@gmail.com>
2021-03-06 16:01:45 -0800

    Completed 05_CheckLetter.cpp
    Finished code to be working nearly perfectly. Still may need to fix so final
    display shows leading zeros

--- csci40/05_CheckLetter.cpp
+++ csci40/05_CheckLetter.cpp
@@ -9,6 +9,38 @@
 
 #include <iostream>
 
-int main() {
+int getCheckLetter(int);
 
+int main() {
+    int checkNum;
+    char checkLetter;
+
+    std::cout << "Enter the check number (8 digits): ";
+    std::cin >> checkNum;
+
+    checkLetter = 'A' + getCheckLetter(checkNum);
+
+    std::cout << checkNum << checkLetter << '\n';
+
+    return 0;
+}
+
+int getCheckLetter(int num) {
+    const int ALPH_NUM = 26;
+    int first, last,
+	mid_f, mid_l,
+	sum, intLetter;
+
+    first = num / 10000;
+    mid_f = first % 100;
+    first = first / 100;
+
+    last = num % 10000;
+    mid_l = last / 100;
+    last = last % 100;
+
+    sum = first + mid_f + mid_l + last;
+    intLetter = sum % ALPH_NUM;
+
+    return intLetter;
 }
