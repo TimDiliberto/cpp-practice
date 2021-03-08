71360c82812155ee2124bb8e255718a84ace9c7e
Timbo <timothydiliberto19@gmail.com>
2021-03-08 12:16:09 -0800

    Complete printWSetAverage function
    Function calculates, prints, and displays  weighted averages for all students
    correctly.

--- csci40/07_Exams.cpp
+++ csci40/07_Exams.cpp
@@ -8,14 +8,15 @@
  */
 
 #include <iostream>
+#include <iomanip>
 
 // Global constants
 const int NUM_STUDENTS = 10;
 const int NUM_TESTS = 4;
 
 // Function prototypes
-void printWSetAverage();
-void printUWTestAverage();
+void printWSetAverage(int [][NUM_TESTS], double []);
+void printUWTestAverage(int [][NUM_TESTS], double []);
 
 
 
@@ -32,17 +33,26 @@ int main() {
 					   {  60,  75, 70, 75  }};
     double weights[NUM_TESTS] = { 0.1, 0.25, 0.3, 0.35 };
 
-    printWSetAverage();
-    printUWTestAverage();
+    printWSetAverage(grades, weights);
+    printUWTestAverage(grades, weights);
 
     return 0;
 }
 
-void printWSetAverage() {
-    std::cout << "Called printWSetAverage() successfully!\n";
+void printWSetAverage(int grades[][NUM_TESTS], double weights[]) {
+    std::cout << "---Weighted Averages---\n";
+    for (int student = 0; student < NUM_STUDENTS; ++student) {
+	double average = 0;
+
+	std::cout << "Student " << student + 1 << ": ";
+	for (int test = 0; test < NUM_TESTS; ++test) {
+	    average += (grades[student][test] * weights[test]);
+	}
+	std::cout << std::fixed << std::setprecision(1) << average << '\n';
+    }
 }
 
-void printUWTestAverage() {
-    std::cout << "Called printUWTestAverage() successfully!\n";
+void printUWTestAverage(int grades[][NUM_TESTS], double weights[]) {
+    std::cout << "---Called printUWTestAverage() successfully!---\n";
 
 }
