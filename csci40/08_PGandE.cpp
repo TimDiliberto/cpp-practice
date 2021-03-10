617c95984a355bd495bfa093fb6c554f227db1e0
Timbo <timothydiliberto19@gmail.com>
2021-03-09 18:41:27 -0800

    Lay framework of 08_PGandE.cpp
    Setup the main function to accept user input for the various values.
    
    Created the calcCharges() function which will calculate the charges for each
    meter, regardless whether it's gas or electric. Sample output has been
    included to test functionality.

--- csci40/08_PGandE.cpp
+++ csci40/08_PGandE.cpp
@@ -1,12 +1,50 @@
 /**
  * Program: PGandE.cpp
  * Written by: Timothy Diliberto
- * 
+ * Takes starting meter readings for gas and electric then determines the
+ *     price using a baseline for usage - below baseline is one price and
+ *     usage above baseline is another.
  * Last modified: 3/9/2021
  */
 
 #include <iostream>
 
-int main() {
+const double GAS_BASE = 31.0,     // therms
+             GAS_UNDER = 0.504,
+             GAS_OVER = 0.824,
+             ELEC_BASE = 238.7,   // kilowatt hours
+             ELEC_UNDER = 0.094,
+             ELEC_OVER = 0.133;
 
-} 
+//Function prototypes
+void calcCharges(double, double);
+
+int main() {
+    double startGas,
+           endGas,
+           startElec,
+           endElec;
+
+    std::cout << "Enter starting and ending readings for gas meter\n"
+              << "Starting Gas: ";
+    std::cin >> startGas;
+    std::cout << "Ending Gas: ";
+    std::cin >> endGas;
+    calcCharges(startGas, endGas);
+
+    std::cout << "\nEnter starting and ending readings for electric meter\n"
+              << "Starting Electric: ";
+    std::cin >> startElec;
+    std::cout << "Ending Electric: ";
+    std::cin >> endElec;
+    calcCharges(startElec, endElec);
+
+    return 0;
+}
+
+void calcCharges(double start, double end) {
+    std::cout << "---Called calcCharges successfully!---\n"
+              << "Testing global constants: " << GAS_BASE << '\n'
+              << "Testing parameters: " << start << end << '\n'
+              << "Exiting function...\n";
+}
