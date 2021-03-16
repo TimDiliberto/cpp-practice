bdb72f9838836a77dbbc5afc1389cf9a62c07f22
Timbo <timothydiliberto19@gmail.com>
2021-03-16 02:46:26 -0700

    Finalized Arrays
    Arrays.cpp (2.0)
     -Added swap function
     -Updated file comments to adhere to Doxygen standards
     -Organized code into search, sort, and util categories
     -Fixed issue of not sorting final value in selectionSort
    
    Arrays.h
     -Updated function comments to adhere to Doxygen standards
     -May need to move function comments to Arrays.cpp in the future

--- Arrays.cpp
+++ Arrays.cpp
@@ -1,14 +1,25 @@
 /**
- * Program: Arrays.cpp
- * Written by: Timothy Diliberto
  * Includes numerous search and sort algorithms for built-in C++ arrays
- *     which can be used fluidly in other programs.
- * Last modified: 3/14/2021
+ *     which can be used fluidly in other programs
+ *
+ * @file Arrays.cpp
+ * @author Timothy Diliberto
+ * @version 2.0 3/14/2021
  */
 
 #include "Arrays.h"
 
-// SEARCH ALGORITHMS
+/**
+ * Simple swaping function that trades positions of the two arguments
+ *
+ * @param &a Reference to first element to be swapped
+ * @param &b Reference to second element to be swapped
+ */
+void swap(int &, int &);
+
+//*******************//
+// SEARCH ALGORITHMS //
+//*******************//
 int linearSearch(int arr[], int size, int val) {
     int index = 0,
         pos = -1;
@@ -45,31 +56,42 @@ int binarySearch(int arr[], int size, int val) {
     return pos;
 }
 
-// SORT ALGS
+//*****************//
+// SORT ALGORTIHMS //
+//*****************//
 void bubbleSort(int arr[], int size) {
     for (int last = size - 1; last > 0; --last) {
-        for (int i = 0; i < last; ++i) {
-            if (arr[i] > arr[i+1]) {
-                int temp = arr[i+1];
-                arr[i+1] = arr[i];
-                arr[i] = temp;
+        for (int index = 0; index < last; ++index) {
+            if (arr[index] > arr[index+1]) {
+                swap(arr[index], arr[index+1]);
             }
         }
     }
 }
 
 void selectionSort(int arr[], int size) {
-    for (int i = 0; i < size - 2; ++i) {
-        int minIndex = i;
-        int minVal = arr[i];
-        for (int j = i+1; j < size - 1; ++j) {
-            if (arr[j] < arr[i]) {
-                minVal = arr[j];
-                minIndex = j;
+    int minIndex,
+        minVal;
+
+    for (int start = 0; start < size - 1; ++start) {
+        minIndex = start;
+        minVal = arr[start];
+        
+        for (int index = start + 1; index < size; ++index) {
+            if (arr[index] < minVal) {
+                minIndex = index;
+                minVal = arr[index];
             }
         }
-    int temp = arr[minIndex];
-    arr[minIndex] = arr[i];
-    arr[i] = temp;
+        swap(arr[start], arr[minIndex]);
     }
+}
+
+//*************//
+// EXTRA UTILS //
+//*************//
+void swap(int &a, int &b) {
+    int temp = a;
+    a = b;
+    b = temp;
 }
--- Arrays.h
+++ Arrays.h
@@ -3,17 +3,45 @@
 
 /**
  * Sequentially searches through an array for a specified value
+ *
  * @param arr The array to be searched
  * @param size The size of the array
- * @param val The value being searched for.
- * @return Index of searched value. If not in array, return -1.
+ * @param val The value being searched for
+ *
+ * @return Index of searched value. If not in array, return -1
  */
 int linearSearch(int [], int, int);
 
+/**
+ * Searches through an array by checking the middle element then
+ * determining whether the sought after value will be before or after
+ * the middle element
+ *
+ * @param arr The array to be searched
+ * @param size The size of the array
+ * @param val The value being searched for
+ *
+ * @return Index of searched value. If not in array, return -1
+ */
 int binarySearch(int [], int, int);
 
+/**
+ * Sorts the given array in ASCENDING order by shifting larger elements
+ * toward the end of the array, one at a time
+ *
+ * @param arr The array to be sorted
+ * @param size The size of the array
+ */
 void bubbleSort(int [], int);
 
+/**
+ * Sorts the given array in ASCENDING order by sending the smallest
+ * element to the first position then sending each next greatest element
+ * to the following position
+ *
+ * @param arr The array to be sorted
+ * @param size The size of the array
+ */
 void selectionSort(int [], int);
 
 #endif
