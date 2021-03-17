a6f7b12fc0626832fd5a59a9f5fd3d2e5685a1f5
Timbo <timothydiliberto19@gmail.com>
2021-03-17 13:14:23 -0700

    Lay framework for 09_SortBenchmarks.cpp
    Setup the general layout for the program, including function prototypes,
    documentation, and initial variables.

---
+++ Gaddis/ch08/09_SortBenchmarks.cpp
@@ -0,0 +1,51 @@
+/**
+ * desc
+ *
+ * @file 09_SortBenchmarks.cpp
+ * @author Timothy Diliberto
+ * @version 1.0 3/17/2021
+ */
+
+#include <iostream>
+
+/**
+ * desc
+ *
+ * @param arr Array to be sorted
+ * @param size Size of the array
+ */
+void bubbleSort(int [], int);
+
+/**
+ * desc
+ *
+ * @param arr Array to be sorted
+ * @param size Size of the array
+ */
+void selectionSort(int [], int);
+
+
+
+// Main process
+int main() {
+    const int SIZE = 20;
+    int exchanges;
+    int arr1[SIZE] = { 76, 87, 96, 8, 41, 1, 68, 54, 13, 70, 30, 58, 56,
+                      82, 53, 29, 39, 22, 80, 19};
+    int arr2[SIZE] = { 76, 87, 96, 8, 41, 1, 68, 54, 13, 70, 30, 58, 56,
+                      82, 53, 29, 39, 22, 80, 19};
+
+    return 0;
+}
+
+/**
+ * desc
+ */
+void bubbleSort(int arr[], int size) {
+} 
+
+/**
+ * desc
+ */
+void selectionSort(int arr[], int size) {
+}
